0fb912848d3821fb8f2dc7b7060136e6c3fbd9cd
andreij6 <andreij6@gmail.com>
2014-07-09 21:27:17 -0500

    angular routes started

---
+++ RecipeApp.Web/Scripts/_app.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+var recipeApp = angular.module('recipeApp', ['ngRoute']);
+
+recipeApp.config(function ($routeProvider) {
+    $routeProvider.when('/',
+            {
+                templateUrl: "/Templates/Home.html",
+                controller: "HomeCtrl"
+            }
+        ).otherwise
+        (
+            { redirectTo: '/' }
+        );
+})
