3ea155efe1f4160b269ae68be8eaa766fc3ed403
andreij6 <andreij6@gmail.com>
2014-07-09 23:54:24 -0500

    added some styling

--- RecipeApp.Web/App_Start/BundleConfig.cs
+++ RecipeApp.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace RecipeApp.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/Fancy.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
